7d01f8f43d6df2fadbb4b304f85c3ece60a5f72c
Brackly Murunga <khanbrackly@gmail.com>
2023-05-11 08:05:41 +0000

    working maria db integration

--- controllers/c2bController.js
+++ controllers/c2bController.js
@@ -1,6 +1,52 @@
 const axios = require('axios')
 require('dotenv').config();
+const mariadb = require('mariadb');
+const moment = require('moment-timezone');
 
+const createTime = () =>{
+  const now = moment().tz('Africa/Nairobi');
+  return now.toISOString();
+}
+
+async function insertPayment(payment,Reconciled) {
+  let conn;
+  try {
+    conn = await mariadb.createConnection({
+      host: process.env.HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DATABASE,
+      port: process.env.DATABASE_PORT
+    });
+
+    const sql = `INSERT INTO payments 
+                 (TransID,TransactionType, TransTime, TransAmount, BusinessShortCode, BillRefNumber, InvoiceNumber, ThirdPartyTransID, MSISDN, FirstName, MiddleName, LastName,Reconciled)
+                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    const values = [
+      payment.TransID,
+      payment.TransactionType,
+      createTime(),
+      parseFloat(payment.TransAmount),
+      payment.BusinessShortCode,
+      payment.BillRefNumber,
+      payment.InvoiceNumber,
+      payment.ThirdPartyTransID,
+      payment.MSISDN,
+      payment.FirstName,
+      payment.MiddleName,
+      payment.LastName,
+      Reconciled
+    ];
+
+    const result = await conn.query(sql, values);
+    console.log(`Inserted payment with ID ${result.insertId}`);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (conn) conn.end();
+  }
+}
 
 const RegisterUrl = async (req, res) => {
   const url = `${req.endpoint_url}/mpesa/c2b/v1/registerurl`
@@ -8,7 +54,7 @@ const RegisterUrl = async (req, res) => {
   if (!ResponseType){
     ResponseType = "Completed"
   }
-  const data=   {    
+  const data =   {    
                     "ShortCode": ShortCode,
                     "ResponseType":ResponseType,
                     "ConfirmationURL":ConfirmationURL,
@@ -35,19 +81,19 @@ const RegisterUrl = async (req, res) => {
 
   // Callback url
 const callback = async (req,res) => {
-  const {TransAmount,BusinessShortCode,BillRefNumber,TransTime} = req.body
-  
+  const {TransAmount,BillRefNumber} = req.body
+  let Reconciled=false;
   const req_data = {
     "currency": "KES",
     "entityId": "001",
-    "transactionDate": "2023-05-03T10:53:23.853Z",
+    "transactionDate": createTime(),
     "partTrans": [
       {
         "accountType": "string",
         "acid": BillRefNumber,
         "partTranType": "Debit",
         "transactionAmount": TransAmount,
-        "transactionDate": "2023-05-03T10:53:23.853Z",
+        "transactionDate": createTime(),
         "transactionParticulars": "string"
       },
     {
@@ -55,16 +101,15 @@ const callback = async (req,res) => {
         "acid": "270000",
         "partTranType": "Credit",
         "transactionAmount": TransAmount,
-        "transactionDate": "2023-05-03T10:53:23.853Z",
+        "transactionDate": createTime(),
         "transactionParticulars": "string"
       }
     ],
     "transactionType": "Transfer",
     "totalAmount": TransAmount
   }
-  
   try{
-    const {data} = await axios(
+    const response = await axios(
       {
         "method":"Post",
         "url": process.env.TRANSACTION_URL,
@@ -77,9 +122,12 @@ const callback = async (req,res) => {
         }
         
     });
-    res.status(data.statusCode).json(data);
+    Reconciled=true
+    insertPayment(req.body,Reconciled);
+    res.status(200).json({'message':'Transaction saved and reconciled'});
   }catch(err){
-    console.log(err.message)
+    insertPayment(req.body,Reconciled);
+    res.status(200).json({message:"Transaction saved but not reconciled"})
   }
   };
 
